8509ec0e927791b357fd850170e9c033abb0e4a3
sky777666 <lmyer12@my.wgu.edu>
2020-06-30 15:47:23 -0700

    added Nav Bar to ejs page

--- controllers/auth.js
+++ controllers/auth.js
@@ -76,5 +76,10 @@ router.post('/login', passport.authenticate('local', {
     failureFlash: 'Invalid username or password.'
 }));
 
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+})
+
 
 module.exports = router;
