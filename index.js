bcb545725eb2e4d535b69369a19b6aa6f0464074
sky777666 <lmyer12@my.wgu.edu>
2020-06-30 14:46:55 -0700

    sessions middleware added

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const ejsLayouts = require("express-ejs-layouts");
 const helmet =require('helmet');
 const session = require("express-session");
 const flash = require("flash");
-
-
+const passport = ('./config/ppConfig')
+const db = require('.models')
+// Want add a link to our customer middleware for isLoggedIn
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 // App Set UP
 const app = Express();
@@ -18,6 +20,33 @@ app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 app.use(helmet());
+
+//create new instance f class sequelize store
+const sessionStore = new SequelizeStore({
+    db: db.sequelize,
+    expiration: 1000 * 60 * 30
+})
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true
+}))
+
+sessionStore.sync();
+
+// Init and Link flash message and passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(function(req, res, next) {
+    res.locals.alert = req.flash();
+    res.locals.currentUser = req.user;
+
+    next();
+});
 
 // ROUTES
 app.get('/', function(req, res) {
